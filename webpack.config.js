1e353735cc86cddffb6646af7d386736b795a44a
twegestal <96296150+twegestal@users.noreply.github.com>
2024-05-07 19:52:28 +0200

    la till webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                context: __dirname + '/src/',
+                from: '*.html'
+            }]
+        })
+    ]
+};
