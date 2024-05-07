ee5c6d242fa0dd9b2e0627ee0cc9c999c36d83f5
twegestal <96296150+twegestal@users.noreply.github.com>
2024-05-07 20:41:24 +0200

    jestie testie

--- src/stack.js
+++ src/stack.js
@@ -1,18 +1,18 @@
-const _ = require('underscore');
+const _ = require("underscore");
 
 let stack = [];
 
 // Lägger ett element överst i stacken
 exports.push = function (x) {
-    stack.push(x);
+  stack.push(x);
 };
 
 // Returnerar det översta elementet i stacken och tar bort det
 exports.pop = function () {
-    return stack.pop();
-}
+  return stack.pop();
+};
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-    return _.last(stack);
-}
+  return _.last(stack);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require('path');
+const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [{
-                context: __dirname + '/src/',
-                from: '*.html'
-            }]
-        })
-    ]
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: __dirname + "/src/",
+          from: "*.html",
+        },
+      ],
+    }),
+  ],
 };
